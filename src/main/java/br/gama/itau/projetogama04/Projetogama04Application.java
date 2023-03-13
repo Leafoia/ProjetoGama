bf44b526131935d04f9d37bad41b8c208cdd1ea9
alanPimenta <alanpimentag@gmail.com>
2023-03-13 15:07:27 -0300

    commit inicial

---
+++ src/main/java/br/gama/itau/projetogama04/Projetogama04Application.java
@@ -0,0 +1,14 @@
+package br.gama.itau.projetogama04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetogama04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetogama04Application.class, args);
+		System.out.println("o");
+	}
+
+}
---
+++ src/test/java/br/gama/itau/projetogama04/Projetogama04ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gama.itau.projetogama04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetogama04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
