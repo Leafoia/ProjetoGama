c100a2ba3fd9cb65f3bfe269eec06b06e70c32fc
alanPimenta <alanpimentag@gmail.com>
2023-03-14 12:27:20 -0300

    Service alterar dados e buscar contas pelo cliente

--- src/main/java/br/gama/itau/projetogama04/repo/ContaRepo.java
+++ src/main/java/br/gama/itau/projetogama04/repo/ContaRepo.java
@@ -1,9 +1,14 @@
 package br.gama.itau.projetogama04.repo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.gama.itau.projetogama04.model.Conta;
 
 public interface ContaRepo extends CrudRepository<Conta, Long> {
-    
+    @Query("select c from Cliente c join c.conta where c.idCliente = :idCliente") //faz o join do Cliente com a Conta onde usamos o idCliente como parâmetro
+
+    List<Conta> getContasByCliente(long idCliente);
 }
--- src/main/java/br/gama/itau/projetogama04/service/ContaService.java
+++ src/main/java/br/gama/itau/projetogama04/service/ContaService.java
@@ -1,5 +1,53 @@
 package br.gama.itau.projetogama04.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import br.gama.itau.projetogama04.exception.NotFoundException;
+import br.gama.itau.projetogama04.model.Conta;
+import br.gama.itau.projetogama04.repo.ContaRepo;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ContaService {
     
+    private final ContaRepo repo;
+
+    public Conta newConta(Conta novaConta) {
+        if(novaConta.getNumeroConta() > 0) {
+            return null;
+        }
+        Conta contaInserida = repo.save(novaConta);
+        return contaInserida;
+    }
+
+    public Conta getById(long numeroConta) {
+
+        Optional<Conta> contaOptional = repo.findById(numeroConta);
+
+        if(contaOptional.isEmpty()) {
+            throw new NotFoundException("Conta não encontrada.");
+        }
+        Conta contaEncontrada = contaOptional.get();
+        return contaEncontrada;
+        
+    }
+
+    public Conta alterarDados (double saldo,long numeroConta ,Conta conta) {
+        Optional<Conta> contaOptional = repo.findById(numeroConta);
+
+        if(contaOptional.isEmpty()) {
+            return null;
+        }
+        conta.setSaldo(saldo);
+        Conta contaAtualizada = repo.save(conta);
+        return contaAtualizada;
+    }
+
+    public List<Conta> buscarContasPeloCliente (long idCliente) {
+        return repo.getContasByCliente(idCliente);
+    }
 }
